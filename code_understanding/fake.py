791f73a207dedd42af7f7a2156db1a5637c09cce
Gabe Dottl <gabedottl@gmail.com>
2019-02-21 16:09:01 -0800

    Add sample fake source for AST analysis

---
+++ code_understanding/fake.py
@@ -0,0 +1,17 @@
+# Comment.
+print(f'executed {__file__}')
+
+class Foo:
+  def __init__(self):
+    self.monster = None
+    pass
+
+  def bar(self):
+    zoo = 'function'
+    print(zoo)
+
+def bar():
+  pass
+
+zoo = 'module'
+print(zoo)
