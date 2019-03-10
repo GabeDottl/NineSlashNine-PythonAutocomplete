48d16a116a71ebeffbfef0de3c619cae468fc392
gabedottl@gmail.com <gabedottl@gmail.com>
2019-03-10 15:06:40 -0700

    Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='autocomplete',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['naive_suggestion_sort', 'reverse_history'],
+      zip_safe=False)
