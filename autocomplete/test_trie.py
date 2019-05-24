ad27285a5763c233661d2bbfdae79813e0a76a61
gabedottl@gmail.com <gabedottl@gmail.com>
2019-05-24 11:44:57 -0700

    Improvements to Trie to support file history tracking

--- autocomplete/test_trie.py
+++ autocomplete/test_trie.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import os
+import glob
 
 from . import trie
 
@@ -7,23 +9,48 @@ def test_trie():
   examples = ['abcde', 'abcde', 'abcdf', 'abc', 'abcdfg', 'qres', 'abcde', 'abc', 'abd', 'abd', 'abde']
   counter = Counter(examples)
   counts = dict(zip(counter.keys(), counter.values()))
-  t = trie.AutocompleteTrie()
+
+  t = trie.Trie()
   for example in examples:
-    t.add(example)
-  value = t.get_best('a')
+    t.add(example, 1, add_value=True)
 
+  value = t.get_max('a')
   assert value == 'abcde', value
-  assert t.get_frequency(value) == counts[value]
-  assert t.get_best('n') is None
-  value = t.get_best('abd')
-  assert value == 'abd'
-  assert t.get_frequency(value) == counts[value]
-  value = t.get_best('q')
-  assert value == 'qres'
-  assert t.get_frequency(value) == counts[value]
+  assert t.get_value_for_string(value) == counts[value]
+  assert t.get_max('n') is None
 
-  t.prune_infrequent_copy(2)
+  value = t.get_max('abd')
+  assert value == 'abd'
+  assert t.get_value_for_string(value) == counts[value]
+
+  value = t.get_max('q')
+  assert value == 'qres'
+  assert t.get_value_for_string(value) == counts[value]
+
+  t.copy_with_lower_values_pruned(2)
+
+def test_trie_with_file_tree():
+  base_dir = os.path.join(os.getenv('CODE'), 'autocomplete')#, 'autocomplete', 'code_understanding', 'typing')
+  assert os.path.exists(base_dir)
+  paths = glob.glob(os.path.join(base_dir, '**','*.py'), recursive=True)
+  directories = set()
+  t = trie.Trie()
+  for path in paths:
+    full_path = os.path.join(base_dir, path)
+    directories.add(os.path.dirname(full_path))
+    t.add(full_path, value=os.path.getmtime(full_path))
+
+  # OS should be essentially doing similar behaviour with getmtime for directories - check.
+  # print(t)
+  for directory in directories:
+    # Cannot guarantee this because we recurse down the tree whereas getmtime doesn't.
+    # assert t.get_max_value_at_or_beneath(directory) <= os.path.getmtime(directory)
+    max_file = t.get_max(directory)
+    assert max_file
+    assert t.get_max_value_at_or_beneath(directory) == os.path.getmtime(max_file)
+
 
 
 if __name__ == '__main__':
   test_trie()
+  test_trie_with_file_tree()
--- autocomplete/trie.py
+++ autocomplete/trie.py
@@ -1,52 +1,52 @@
 '''This class encapsulates a Trie with some optimizations and persistence abilities.
 
-Classic tries for autocomplete may include a count for leaf-nodes and a pointer to the child-subtree
-with the highest-value leaf node in it.
+Classic tries for autocomplete may include a frequency for leaf-nodes and a pointer to the
+child-subtree with the highest-frequency leaf node in it.
 
 This Trie implementation generalizes this slightly to 'comparable' objects - i.e. objects which
 support >,< and ==.
 
 Furthermore, the Trie supports saving/loading from a file using msgpack.
+
+In exchange for some complexity, this Trie implementation is more efficient than traditional tries
+by allowing nodes to contain whole strings where it is useful to do so. This is particularly
+valuable when the trie would otherwise be sparse at many nodes - for example, with file-trees.
 '''
 import msgpack
-
 
 class Node:
   def __init__(self):
     self.children = {}
-    self.count = 0
-    self.highest_child_count = 0
+    self.value = 0
+    self.highest_child_value_at_or_beneath = 0
     self.highest_child_char = ''
     self.remainder = ''
-
-  def increment_count(self):
-    self.count += 1
 
   def handle_child_increase(self, char):
     if self.highest_child_char == char:
       return
     child = self.children[char]
-    child_max_count = child.get_max_count_at_or_beneath()
-    if child_max_count > self.highest_child_count:
+    child_max_value = child.get_max_value_at_or_beneath()
+    if child_max_value > self.highest_child_value_at_or_beneath:
       self.highest_child_char = char
-      self.highest_child_count = child_max_count
+      self.highest_child_value_at_or_beneath = child_max_value
 
-  def get_max_count_at_or_beneath(self):
-    return max(self.count, self.highest_child_count)
+  def get_max_value_at_or_beneath(self):
+    return max(self.value, self.highest_child_value_at_or_beneath)
 
-  def prune_infrequent_copy(self, min_count):
+  def copy_with_lower_values_pruned(self, min_value) -> 'Node':
     out = Node()
-    if self.highest_child_count >= min_count:
-      out.highest_child_count = self.highest_child_count
+    if self.highest_child_value_at_or_beneath >= min_value:
+      out.highest_child_value_at_or_beneath = self.highest_child_value_at_or_beneath
       out.highest_child_char = self.highest_child_char
     for char, child in self.children.items():
-      if child.get_max_count_at_or_beneath() >= min_count:
-        out.children[char] = child.prune_infrequent_copy(min_count)
+      if child.get_max_value_at_or_beneath() >= min_value:
+        out.children[char] = child.copy_with_lower_values_pruned(min_value)
     return out
 
   def traverse(self, array=False, nodes=None, path=''):
     class LoopException(Exception):
-      pass
+      ...
 
     char = ''
     try:
@@ -55,12 +55,12 @@ class Node:
 
       nodes.add(self)
       for char, child in self.children.items():
-        if child.count > 0:
-          yield child.count, [char, child.remainder] if array else char + child.remainder
+        if child.value > 0:
+          yield child.value, [char, child.remainder] if array else char + child.remainder
         if child in nodes:
           raise LoopException(char)
-        for count, arr in child.traverse(array=True, nodes=nodes, path=path + char):
-          yield count, [char, child.remainder] + arr if array else ''.join([char, child.remainder] + arr)
+        for value, arr in child.traverse(array=True, nodes=nodes, path=path + char):
+          yield value, [char, child.remainder] + arr if array else ''.join([char, child.remainder] + arr)
     except LoopException as e:
       raise LoopException(''.join(e.args + [char]))
     except RecursionError as e:
@@ -69,14 +69,17 @@ class Node:
     nodes.remove(self)
 
   def to_str(self, indent=''):
-    return ''.join(f'{indent}{c}{node.remainder}\n{node.to_str(indent + " "*(1+len(node.remainder)))}'
+    return ''.join(f'{indent}{c}{node.remainder} ({str(node.value)})\n{node.to_str(indent + " "*(1+len(node.remainder)+ 2 + len(str(self.value))))}'
                    for c, node in self.children.items())
+
+  def __str__(self):
+    return self.to_str()
 
   def add_child(self, char, child):
     assert child != self, char
-    child_max_count = child.get_max_count_at_or_beneath()
-    if self.highest_child_count < child_max_count:
-      self.highest_child_count = child_max_count
+    child_max_value = child.get_max_value_at_or_beneath()
+    if self.highest_child_value_at_or_beneath < child_max_value:
+      self.highest_child_value_at_or_beneath = child_max_value
       self.highest_child_char = char
     self.children[char] = child
 
@@ -89,7 +92,7 @@ class Node:
         return path
       i = min(len(curr_node.remainder), len(sub_str))
       if i > 0 and sub_str[:i] != curr_node.remainder[:i]:
-        return None
+        break
       if i == len(sub_str):
         return path
       try:
@@ -97,13 +100,13 @@ class Node:
         path.append((sub_str[i], curr_node))
         sub_str = sub_str[i + 1:]
       except KeyError:
-
-        return None
+        break
+    return None
 
   def get_max_path(self):
     curr_node = self
     chars = []
-    while curr_node.count < curr_node.highest_child_count:
+    while curr_node.value < curr_node.highest_child_value_at_or_beneath:
       chars.append(curr_node.highest_child_char)
     return ''.join(chars)
 
@@ -115,13 +118,16 @@ class Node:
     return True
 
 
-class AutocompleteTrie:
+class Trie:
   def __init__(self):
     self.root = Node()
 
-  def prune_infrequent_copy(self, min_count):
-    out = AutocompleteTrie()
-    out.root = self.root.prune_infrequent_copy(min_count)
+  def __str__(self):
+    return str(self.root)
+
+  def copy_with_lower_values_pruned(self, min_value):
+    out = Trie()
+    out.root = self.root.copy_with_lower_values_pruned(min_value)
     return out
 
 
@@ -147,20 +153,39 @@ class AutocompleteTrie:
 #
 # Insert partially into
 
-  def add(self, line):
-    def split_node(node, split_point, additional_remainder):
+  def add(self, string, value, add_value=False):
+    def split_node(node, split_point, additional_remainder, additional_remainder_value):
+      '''Takes an existing node with some string and splits it in its remainder string.
+
+      The original node is preserved, just with it's remainder cut and it's new-parent is returned
+      (with another child beneath said parent if additional_remainder is not '').
+
+      For example:
+      node='Google'
+      split_point=1 # Go
+      additional_remainder = 'al' # Goal
+      Alternatively, additional_remainder may be '' in which case instead of 3 nodes, there
+      are now 2 with 'Go' as a new possible end-point.
+      '''
       new_node = Node()
       new_node.remainder = node.remainder[:split_point]
       c = node.remainder[split_point]
-      new_node.highest_child_char = c
-      new_node.highest_child_count = node.get_max_count_at_or_beneath()
-      new_node.children[c] = node
+      # new_node.value = self.default_value - TODO
+      new_node.add_child(c, node)
       node.remainder = node.remainder[split_point + 1:]
       if len(additional_remainder) > 0:
         new_child = Node()
-        new_child.remainder = additional_remainder[1:]
-        new_child.count = 1
+        new_child.remainder = additional_remainder[1:] # First char used for indexing below.
+        new_child.value = additional_remainder_value
         new_node.add_child(additional_remainder[0], new_child)
+        if additional_remainder_value > node.get_max_value_at_or_beneath():
+          new_node.highest_child_value_at_or_beneath = additional_remainder_value
+          new_node.highest_child_char = additional_remainder[0]
+
+      if not new_node.highest_child_char:  # No remainder or remainder value's smaller.
+        new_node.highest_child_value_at_or_beneath = node.get_max_value_at_or_beneath()
+        new_node.highest_child_char = c
+      
       return new_node
 
     class RemainderSplitException(Exception):
@@ -171,25 +196,25 @@ class AutocompleteTrie:
     curr_node = self.root
     path = []
 
-    line_iter = iter(line)
+    string_iter = iter(string)
     b = ''
     c = ''
     try:
-      for c in line_iter:
+      for c in string_iter:
         path.append((c, curr_node))
         # Reset split point in case of exception before natural reset.
         split_point = 0
         curr_node = curr_node.children[c]
-        for split_point, (a, b) in enumerate(zip(curr_node.remainder, line_iter)):
+        for split_point, (a, b) in enumerate(zip(curr_node.remainder, string_iter)):
           if a != b:
             raise RemainderSplitException(split_point=split_point)
       if split_point > 0 and split_point != len(curr_node.remainder) - 1:
-        new_subtree = split_node(curr_node, split_point + 1, '')
-        new_subtree.count = 1
+        new_subtree = split_node(curr_node, split_point + 1, '', 0)  # TODO: self.default_value
+        new_subtree.value = value
         c, parent = path[-1]
         parent.children[c] = new_subtree
-      else:  # Perfect match with curr_node - increment count.
-        curr_node.increment_count()
+      else:  # Perfect match with curr_node - increment value.
+        curr_node.value = value if not add_value else curr_node.value + value
         last_node = curr_node
         x = []
         for c, n in path:
@@ -198,36 +223,60 @@ class AutocompleteTrie:
         x.append(curr_node.remainder)
         x = ''.join(x)
         for c, node in path[::-1]:
-          last_node_max_count = last_node.get_max_count_at_or_beneath()
-          if node.highest_child_count < last_node_max_count:
-            node.highest_child_count = last_node_max_count
+          last_node_max_value = last_node.get_max_value_at_or_beneath()
+          if node.highest_child_value_at_or_beneath < last_node_max_value:
+            node.highest_child_value_at_or_beneath = last_node_max_value
             node.highest_child_char = c
           else:
             # If it's not highest at this level in the tree, it won't be further
             # up.
             break
     except RemainderSplitException as e:
-      # Partially in remainder. Replace curr_node in tree with new subtree.
-      # No need to update highest-values.
+      # Partially in remainder. Replace curr_node in tree with new subtree. curr_node will split
+      # into 3 nodes.
+      # TODO: update highest-values.
       prefix = ''.join(x[0] for x in path)
-      remainder = ''.join([b] + list(line_iter))
+      remainder = ''.join([b] + list(string_iter))
       c, parent = path[-1]
-      parent.children[c] = split_node(curr_node, e.split_point, remainder)
+      new_node = parent.children[c] = split_node(curr_node, e.split_point, remainder, value)
+      if value > curr_node.get_max_value_at_or_beneath():
+        # Need to update hierarchy.
+        for c, node in path[::-1]:
+          if node.highest_child_value_at_or_beneath < value:
+            node.highest_child_value_at_or_beneath = value
+            node.highest_child_char = c
+          else:
+            break  # Everything above is greater.
     except KeyError:
       # Matches current node's remainder if any - need to add as new child.
-      remaining_chars = ''.join(list(line_iter))
+      remaining_chars = ''.join(list(string_iter))
       new_node = Node()
       new_node.remainder = remaining_chars
-      new_node.count = 1
+      new_node.value = value
+      old_max = curr_node.get_max_value_at_or_beneath()
       curr_node.add_child(c, new_node)
+      if old_max < value:
+        # Need to update hierarchy.
+        for c, node in path[::-1][1:]:
+          if node.highest_child_value_at_or_beneath < value:
+            node.highest_child_value_at_or_beneath = value
+            node.highest_child_char = c
+          else:
+            break  # Everything above is greater.
 
-  def get_frequency(self, value):
-    nodes = self.root.get_path_to(value)
+  def get_value_for_string(self, string):
+    nodes = self.root.get_path_to(string)
     if not nodes:
       return 0
-    return nodes[-1][1].count
+    return nodes[-1][1].value
 
-  def get_best(self, prefix):
+  def get_max_value_at_or_beneath(self, prefix, default=0):
+    path = self.root.get_path_to(prefix)
+    if not path:
+      return default
+    return path[-1][1].highest_child_value_at_or_beneath
+
+  def get_max(self, prefix):
     path = self.root.get_path_to(prefix)
     if not path:
       return None
@@ -237,7 +286,7 @@ class AutocompleteTrie:
       result_arr.append(c)
       result_arr.append(node.remainder)
     curr_node = path[-1][1]
-    while curr_node.highest_child_count > curr_node.count:
+    while curr_node.highest_child_value_at_or_beneath > curr_node.value:
       result_arr.append(curr_node.highest_child_char)
       curr_node = curr_node.children[curr_node.highest_child_char]
       result_arr.append(curr_node.remainder)
